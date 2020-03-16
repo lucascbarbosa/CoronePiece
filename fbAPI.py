a059964e1585039fad2cd122c658324e95fc2fcd
barbichola0899 <barbichola0899@users.noreply.github.com>
2020-03-16 13:25:02 -0300

    update

--- fbAPI.py
+++ fbAPI.py
@@ -5,18 +5,20 @@ from getEpisodes import getEpisodes
 import facebook 
 
 
-def post(cases):
-    access_token = 'EAAC5zB5is18BAFla93ZCHmGJ9YUn9j4yhCDlsQXPZAg1lvlTUd8PqYNTxihO8opkg6E4bjL8lbT8hFb2Yk50Wo2lpaplng2Fp3ICVRzN76arPQQ1YWiO4ZCfcyNCZCnPmk8m89wTsDUOdSMZCSP2IGRswBt8oTZBRHLCGEjQcYke33cBVwDwWaJF4J2r3SwiIZD'
+def post(cases,episodes):
+    access_token = 'EAAC5zB5is18BAEov3LI7sdUeGkBzdCnCCRFZBBHUYPHirgYW4ryKASVSZBamo4nJNbnw9TYfLgXiK3geahqJjjvPkjd47BcldnAmQBXY5q8JHLTWEuoRhO4wlK3ml9UoiqusvBcoWGyBmQjLw1KoSitRMODuiQEVazLHlnGmGdiX37NvUoDScq2rBgGZB8ZD'
     fb = facebook.GraphAPI(access_token=access_token)
     title = episodes[episodes['Chapter']==str(cases)]['Title'].values[0]
     date = dt.now().strftime("%d/%m/%y")
-    message = 'Dia %s:\n\nNúmero de Casos: %d\n\nEpisódio: %s'%(date,cases,title)
+    time = '{:d}:{:02d}'.format(dt.now().hour,dt.now().minute)
+    message = 'Dia %s %s\n\nNúmero de Casos: %d\n\nEpisódio: %s'%(date,time,cases,title)
     image = open('imgs/%d.jpg'%cases,'rb');
     fb.put_photo(image=image,message=message)    
 
 def main():
     episodes = getEpisodes()
     cases = getCases()
+    post(cases,episodes)
     print('Casos =%d'%cases)
     while True:
         new_cases = getCases()
@@ -24,7 +26,7 @@ def main():
             print('mudou')
             cases = new_cases
             print('Casos =%d'%cases)
-            post(cases)
+            post(cases,episodes)
         else:
             pass
         time.sleep(1)
--- ttAPI.py
+++ ttAPI.py
@@ -8,12 +8,13 @@ from getEpisodes import getEpisodes
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler("RYOP7vU2iJX5BNonpaQkkolqq", "LiugL6Tx7X9dPcH2UK68Yl3Mz5R78vktb1ZDa2gMlPJwUsA9Js")
 auth.set_access_token("1076894136312176640-YAENb5bSxk4eopWy8gcJzEdQkobWNt", "kSOIyg3HWz5IjJj0RYAbHSA2udWq66070BnWUKY34ZikE")
-def post(cases):
+def post(cases,episodes):
     access_token = 'EAAC5zB5is18BAFla93ZCHmGJ9YUn9j4yhCDlsQXPZAg1lvlTUd8PqYNTxihO8opkg6E4bjL8lbT8hFb2Yk50Wo2lpaplng2Fp3ICVRzN76arPQQ1YWiO4ZCfcyNCZCnPmk8m89wTsDUOdSMZCSP2IGRswBt8oTZBRHLCGEjQcYke33cBVwDwWaJF4J2r3SwiIZD'
     api = tweepy.API(auth)
     title = episodes[episodes['Chapter']==str(cases)]['Title'].values[0]
     date = dt.now().strftime("%d/%m/%y")
-    message = 'Dia %s:\n\nNúmero de Casos: %d\n\nEpisódio: %s'%(date,cases,title)
+    time = '{:d}:{:02d}'.format(dt.now().hour,dt.now().minute)
+    message = 'Dia %s %s\n\nNúmero de Casos: %d\n\nEpisódio: %s'%(date,time,cases,title)
     image = 'imgs/%d.jpg'%cases
     api.update_with_media(image,message)
 
@@ -21,14 +22,15 @@ def post(cases):
 def main():
     episodes = getEpisodes()
     cases = getCases()
+    post(cases,episodes)
     print('Casos =%d'%cases)
     while True:
         new_cases = getCases()
         if new_cases != cases:
             print('mudou')
             cases = new_cases
-            print('Casos =%d'%cases)d
-            post(cases)
+            print('Casos =%d'%cases)
+            post(cases,episodes)
         else:
             pass
         time.sleep(1)
